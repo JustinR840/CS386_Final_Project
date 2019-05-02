079f971a72de7856054e5d01dba4cb58ea99e0cd
eaxworthy <eaxworthy@blue.cs.sonoma.edu>
2019-05-02 11:16:32 -0700

    LandingPage.js update# Please enter the commit message for your changes. Lines starting

--- advising_router/src/Pages/Landing/LandingPage.js
+++ advising_router/src/Pages/Landing/LandingPage.js
@@ -1,24 +1,91 @@
 import React, { Component } from 'react';
 import Header from './Header.js'
 import API from "../../APIInterface/APIInterface.js";
-
+import AdviseeView fro '../Advisee'
 import AdvisorView from '../Advisor/AdvisorView.js';
 import {Redirect, withRouter} from "react-router-dom";
 
 
 class LandingPage extends Component
 {
+	constructor(props)
+	{
+		super(props);
+
+		let user = null;
+
+		if(this.props.location.state !== null)
+		{
+			user = this.props.location.state.user;
+		}
+
+		this.state = {
+			user: user
+		};
+	}
+
+
+	getUsername()
+	{
+		// This is all just code to set the username next to the logout button.
+		let fName = this.state.user['fName'];
+		let lName = this.state.user['lName'];
+
+		let userName = "";
+
+		if(fName !== null)
+		{
+			userName += fName;
+			// Try appending lName to the userName also.
+			if(lName !== null)
+			{
+				userName += " " + lName;
+			}
+		}
+		else
+		{
+			// Fallback to using the user_id as a display name if fName is null.
+			userName = this.state.user['user_id'];
+		}
+
+		return userName;
+	}
+
+
+	getHTMLToReturn(role)
+	{
+		let userName = this.getUsername();
+		let AdvisorNames = ['Dr. Zik', 'Dr. Yolopanther', 'Dr. Doc'];
+
+		if(role === "advisor")
+		{
+			return (
+				<div>
+					<Header menuName="Test Pls" itemNames={AdvisorNames} userName={userName} userType={this.state.user['role']}/>
+					<AdvisorView advisor={this.state.user}/>
+				</div>
+			);
+		}
+		else if(role === "advisee")
+		{
+			// TODO: CHANGE ME FOR ADVISEE
+			return (
+				<div>
+					<Header menuName="Test Pls" itemNames={AdvisorNames} userName={userName} userType={this.state.user['role']}/>
+					<AdviseeView advisor={this.state.user}/>
+				</div>
+			);
+		}
+	}
+
 	render()
 	{
-		let AdvisorNames = ['Dr. Zik', 'Dr. Yolopanther', 'Dr. Doc'];
 /*
 		const api = new API();
-
 		let userName = 'Erei';
 		let AdvisorNames = ['Dr. Zik', 'Dr. Yolopanther', 'Dr. Doc'];
 		let user_type = this.props.userType;
 		console.log("hi",this.props);
-
 		if(user_type === 'advisee'){
 			console.log('hi');
 			api.getAdvisor(this.props.user_id).then((info) =>
@@ -32,54 +99,21 @@ class LandingPage extends Component
 		}
 */
 
-		if(this.props.location.state === null || this.props.location.state === undefined)
-		{
-			console.log("Location state is null, redirecting to /login");
-			return <Redirect to="/login"/>
-		}
-
-		let user = this.props.location.state.user;
-
 		// Check if user is logged in
-		if(user === null || user === undefined)
+		if(this.state.user === null)
 		{
 			console.log("User is null, redirecting to /login");
 			// If user is not logged in, redirect them to the login page
 			return <Redirect to="/login"/>;
 		}
-		else
-		{
-			console.log(user);
 
-			// Set username on right side
-			let fName = user['fName'];
-			let lName = user['lName'];
-			let userName = "";
-
-			if(fName !== null)
-			{
-				userName += fName;
-
-				if(lName !== null)
+		return (
+			<div>
 				{
-					userName += " " + lName;
+					this.getHTMLToReturn(this.state.user['role'])
 				}
-			}
-			else
-			{
-				userName = user['user_id'];
-			}
-
-
-			return (
-				<div>
-					<Header menuName="Test Pls" itemNames={AdvisorNames} userName={userName} userType={user['role']}/>
-					<AdvisorView advisor={user}/>
-				</div>
-			);
-
-		}
-
+			</div>
+		);
 	}
 }
 
